3196d4f1a72d96f8a1e0810192d40d51b7c1d908
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-06-24 22:26:37 -0300

    fix(LoginController)
    - Alterações na Controller Login

--- sistema-rematricula/src/main/java/rematricula/controller/LoginController.java
+++ sistema-rematricula/src/main/java/rematricula/controller/LoginController.java
@@ -1,6 +1,7 @@
 package rematricula.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 
+
+
 import rematricula.dao.UsuariosDao;
+import rematricula.model.NivelUsuario;
 import rematricula.model.Usuarios;
 
 @Controller
@@ -19,16 +23,30 @@ public class LoginController {
 	UsuariosDao dao;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String telaLogin() {
+	public String telaLogin(Model model) {
+			model.addAttribute("mensagem", "");
 			return "telaLogin";
 	}
 
-	@RequestMapping(value = "/validaLogin", method = RequestMethod.POST)
-	public String validar(Usuarios usuario, Model model) {
+	@RequestMapping(value = "/loginValida", method = RequestMethod.POST)
+	public String validar(HttpSession session, Usuarios usuario, Model model) {
 		Usuarios usuarioExiste = dao.validaLogin(usuario.getLoginUsuario(), usuario.getSenhaUsuario());
 		if (usuarioExiste != null) {
-				return "index";		
-		}
-		return "index";	
+			session.setAttribute("usuario logado", usuarioExiste);
+			if (usuarioExiste.getNivelUsuario() == NivelUsuario.ADMINISTRADOR){
+				//model.addAttribute("botaoCadastrar", true);
+				return "index";
+			}
+			else if (usuarioExiste.getNivelUsuario() == NivelUsuario.PROFESSOR) {
+				//model.addAttribute("botaoCadastrar", false);
+				return "indexProfessor";
+			} else {
+				//model.addAttribute("botaoCadastrar", false);
+				return "indexAluno";
+			}
+		} else {
+			model.addAttribute("mensagem", "Usuário ou senha inválida");
+			return "telaLogin";
+		}		
 	}
 }
