02556d220ccc2e66bbca2daea2fdcc2f216fc84a
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-07-09 17:21:53 -0300

    fix(Session)
    - Método de autorização da sessão

--- sistema-rematricula/src/main/java/rematricula/AutorizadorInterceptor.java
+++ sistema-rematricula/src/main/java/rematricula/AutorizadorInterceptor.java
@@ -1,27 +1,29 @@
-//package rematricula;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-//
-//public class AutorizadorInterceptor extends HandlerInterceptorAdapter {
-//	@Override
-//	public boolean preHandle(HttpServletRequest request,
-//			HttpServletResponse response, Object controller) throws Exception {
-//
-//		String uri = request.getRequestURI();
-//		if (uri.endsWith("/")||
-//			uri.endsWith("loginValida")||
-//			uri.contains("resources")) {
-//			return true;
-//		}
-//
-//		if (request.getSession().getAttribute("usuario logado") != null) {
-//			return true;
-//		}
-//		
-//		response.sendRedirect("/");
-//		return false;
-//	}
-//}
+package rematricula;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AutorizadorInterceptor extends HandlerInterceptorAdapter {
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object controller) throws Exception {
+
+		String uri = request.getRequestURI();
+		if (uri.endsWith("/")||
+			uri.endsWith("loginValida")||
+			uri.contains("resources") ||
+			uri.endsWith("404")||
+			uri.endsWith("500")) {
+			return true;
+		}
+
+		if (request.getSession().getAttribute("usuario logado") != null) {
+			return true;
+		}
+		
+		response.sendRedirect("/");
+		return false;
+	}
+}
--- sistema-rematricula/src/main/java/rematricula/MvcConfig.java
+++ sistema-rematricula/src/main/java/rematricula/MvcConfig.java
@@ -1,14 +1,14 @@
-//package rematricula;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-//@Configuration
-//public class MvcConfig extends WebMvcConfigurerAdapter {
-//@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(new AutorizadorInterceptor());
-//	}	
-//
-//}
+package rematricula;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new AutorizadorInterceptor());
+	}	
+
+}
